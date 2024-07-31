8bcb7a58fc6799c89c4e990ece0d8f2aaeda7e93
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2024-07-31 18:29:24 +0300

    Update models.py

--- server/models.py
+++ server/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
+
 class Customer(db.Model):
 
     __tablename__ = "customers"
@@ -17,8 +18,7 @@ class Customer(db.Model):
     phone_number = db.Column(db.Integer, nullable=False)
     ID_or_Passport = db.Column(db.Integer, unique=True, nullable=False)
 
-
-    bookings = relationship('Booking', back_populates='customer')
+    bookings = relationship("Booking", back_populates="customer")
 
 
 class Bus(db.Model):
@@ -26,40 +26,36 @@ class Bus(db.Model):
     __tablename__ = "buses"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
-
-
-
-    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'), nullable=False)
+    
+    driver_id = db.Column(db.Integer, db.ForeignKey("drivers.id"), nullable=False)
     cost_per_seat = db.Column(db.Integer, nullable=False)
     number_of_seats = db.Column(db.Integer, nullable=False)
     route = db.Column(db.String, nullable=False)
     travel_time = db.Column(db.DateTime, nullable=False)
 
+    driver = relationship("Driver", back_populates="buses")
+    schedules = relationship("Schedule", back_populates="bus")
+    bookings = relationship("Booking", back_populates="bus")
 
-
-
-    driver = relationship('Driver', back_populates='buses')
-    schedules = relationship('Schedule', back_populates='bus')
-    bookings = relationship('Booking', back_populates='bus')
 
 class Schedule(db.Model):
 
     __tablename__ = "schedules"
     id = db.Column(db.Integer, primary_key=True)
 
-    bus_id = db.Column(db.Integer, db.ForeignKey('buses.id'), nullable=False)
+    bus_id = db.Column(db.Integer, db.ForeignKey("buses.id"), nullable=False)
     departure_time = db.Column(db.DateTime, nullable=False)
     arrival_time = db.Column(db.DateTime, nullable=False)
     travel_date = db.Column(db.DateTime, nullable=False)
+
 
 class Booking(db.Model):
 
     __tablename__ = "bookings"
     id = db.Column(db.Integer, primary_key=True)
 
-
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
-    bus_id = db.Column(db.Integer, db.ForeignKey('buses.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"), nullable=False)
+    bus_id = db.Column(db.Integer, db.ForeignKey("buses.id"), nullable=False)
     booking_date = db.Column(db.DateTime, default=datetime.utcnow)
     number_of_seats = db.Column(db.Integer, nullable=False)
 
@@ -76,6 +72,7 @@ class Driver(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
 
+
 class Admin(db.Model):
 
     __tablename__ = "admin"
@@ -83,5 +80,4 @@ class Admin(db.Model):
     username = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
-
 
