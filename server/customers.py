527bc5bfbb740a1e1d81ac85cf84c0744d6f77ce
Laurine Maina <mainalaureen58@gmail.com>
2024-08-19 10:01:14 +0300

    added roles

--- server/customers.py
+++ server/customers.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request,make_response,session
 from flask_bcrypt import Bcrypt
 from flask_restful import Api, Resource
-from .models import Customer, Booking, db,Bus,Schedule,Seat
+from models import Customer, Booking, db,Bus,Schedule,Seat
 from datetime import datetime
 from flask_jwt_extended import JWTManager,create_access_token,create_refresh_token,get_jwt_identity,jwt_required
 import logging
